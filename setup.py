b869deb308aee5cfe384a265ba386830fe670266
Your Namesharath957 <sharath957@gmail.com>
2021-10-06 14:21:26 +0530

    Training updated

--- setup.py
+++ setup.py
@@ -1,37 +1,24 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
-PROJECT_NAME ="ANN-implementation-DLCVNLP-demo "
-USER_NAME = 'sharath957'
-
-setuptools.setup(
-    name=f"{PROJECT_NAME}-{USER_NAME}", 
+setup(
+    name="src",
     version="0.0.1",
-    author=USER_NAME,
-    author_email="sharath957@gmail.com",
-    description="It is a implementation of ANN",
+    author="sharath957",
+    description="A small package for ANN Implementation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=f"https://github.com/{USER_NAME}/{PROJECT_NAME}",
-    project_urls={
-        "Bug Tracker": f"https://github.com/{USER_NAME}/{PROJECT_NAME}/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    url="https://github.com/sharath957/ANN-implementation-DLCVNLP-demo.git",
+    author_email="sharath957@gmail.com",
+    packages=["src"],
     python_requires=">=3.7",
     install_requires=[
-        "tensorflow", 
-        "matplotlib", 
+        "tensorflow",
+        "matplotlib",
+        "seaborn",
         "numpy",
-        "pandas",
-        "seaborn" 
-        
+        "pandas"
     ]
 )
