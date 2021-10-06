f9d836dd0f6c1e7d179539e9699b955a7b17434c
Your Namesharath957 <sharath957@gmail.com>
2021-10-06 15:15:20 +0530

    Trainig updated

--- src/training.py
+++ src/training.py
@@ -1,12 +1,13 @@
 from src.utils.common import read_config
 from src.utils.data_mgmt import get_data
 from src.utils.model import create_model
+
 import argparse
 
 def training(config_path):
-    config = read_config(config_path) 
-
-    validation_datasize = config['params']['validation_datasize']
+    config = read_config(config_path)
+    
+    validation_datasize = config["params"]["validation_datasize"]
     (X_train, y_train), (X_valid, y_valid), (X_test, y_test) = get_data(validation_datasize)
 
     LOSS_FUNCTION = config["params"]["loss_function"]
@@ -21,15 +22,13 @@ def training(config_path):
 
     history = model.fit(X_train, y_train, epochs=EPOCHS,
                     validation_data=VALIDATION_SET)
-     
-
 
 if __name__ == '__main__':
-    args = argparse.ArgumentParser() 
+    args = argparse.ArgumentParser()
 
-    args.add_argument('--config','-c',default='config.yaml') 
+    args.add_argument("--config", "-c", default="config.yaml")
 
     parsed_args = args.parse_args()
 
     training(config_path=parsed_args.config)
-
+    
