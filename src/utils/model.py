73072f7444f584cfefa1206859191c063c2dd676
Your Namesharath957 <sharath957@gmail.com>
2021-10-10 17:13:22 +0530

    Model creation

--- src/utils/model.py
+++ src/utils/model.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
+import time
+import os
 
 
 def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES):
 
-    LAYERS = [tf.keras.layers.Flatten(input_shape=[28, 28], name="inputLayer"),
-          tf.keras.layers.Dense(300, activation="relu", name="hiddenLayer1"),
-          tf.keras.layers.Dense(100, activation="relu", name="hiddenLayer2"),
-          tf.keras.layers.Dense(NUM_CLASSES, activation="softmax", name="outputLayer")]
+    LAYERS = [tf.keras.layers.Flatten(input_shape=[28, 28], name="input_layer"),
+          tf.keras.layers.Dense(300, activation="relu", name="hidden_layer1"),
+          tf.keras.layers.Dense(100, activation="relu", name="hidden_layer2"),
+          tf.keras.layers.Dense(NUM_CLASSES, activation="softmax", name="output_layer")]
 
     model_clf = tf.keras.models.Sequential(LAYERS)
 
@@ -17,4 +19,14 @@ def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES):
                 metrics=METRICS)
 
     return model_clf ## <<< untrained model
+
+def get_unique_filename(filename):
+    unique_filename = time.strftime(f"%Y%m%d_%H%M%S_{filename}")
+    return unique_filename
+
+def save_model(model, model_name, model_dir):
+    unique_filename = get_unique_filename(model_name)
+    path_to_model = os.path.join(model_dir,unique_filename)
+    model.save(path_to_model)
+
     
