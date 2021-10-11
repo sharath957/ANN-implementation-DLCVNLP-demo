41cbf187e3c16ee543d4ec5f4ce2dd86ab45a1d5
Your Namesharath957 <sharath957@gmail.com>
2021-10-11 11:49:10 +0530

    Added a save model function

--- src/training.py
+++ src/training.py
@@ -4,9 +4,10 @@ from src.utils.model import create_model, save_model
 import os
 import argparse
 
+
 def training(config_path):
     config = read_config(config_path)
-    
+
     validation_datasize = config["params"]["validation_datasize"]
     (X_train, y_train), (X_valid, y_valid), (X_test, y_test) = get_data(validation_datasize)
 
@@ -21,16 +22,16 @@ def training(config_path):
     VALIDATION_SET = (X_valid, y_valid)
 
     history = model.fit(X_train, y_train, epochs=EPOCHS,
-                    validation_data=VALIDATION_SET)
+                        validation_data=VALIDATION_SET)
 
     artifacts_dir = config['artifacts']['artifacts_dir']
     model_dir = config['artifacts']['model_dir']
-    model_dir_path = os.path.join(artifacts_dir,model_dir)
-    os.makedirs(model_dir_path,exist_ok=True)
+
+    model_dir_path = os.path.join(artifacts_dir, model_dir)
+    os.makedirs(model_dir_path, exist_ok=True)
 
     model_name = config['artifacts']["model_name"]
-    save_model(model,model_name,model_dir)
-
+    save_model(model, model_name, model_dir_path)
 
 
 if __name__ == '__main__':
@@ -41,4 +42,3 @@ if __name__ == '__main__':
     parsed_args = args.parse_args()
 
     training(config_path=parsed_args.config)
-    
