77dd59c4f21f04f4053d2ddd0a364d06c05478f9
Your Namesharath957 <sharath957@gmail.com>
2021-10-19 11:15:48 +0530

    Updated

---
+++ src/utils/log.py
@@ -0,0 +1,11 @@
+import time
+import os
+import tensorflow as tf
+def get_log_path(log_dir="logs/fit"):
+  uniqueName = time.strftime("log_%Y_%m_%d_%H_%M_%S")
+  log_path = os.path.join(log_dir, uniqueName)
+  print(f"savings logs at: {log_path}")
+
+  return log_path
+
+
