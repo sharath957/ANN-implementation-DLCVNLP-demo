b180bb451c152426616ca3a4822c661136e6fa62
Your Namesharath957 <sharath957@gmail.com>
2021-10-25 12:29:29 +0530

    Callback implementation

--- src/training.py
+++ src/training.py
@@ -3,9 +3,7 @@ from src.utils.common import read_config
 from src.utils.data_mgmt import get_data
 from src.utils.model import create_model, save_model
 from src.utils.Save_plot import save_plot
-#from src.utils.log import get_log_path
-#import tensorflow as tf
-#import numpy as np
+from src.utils.callbacks import get_callbacks 
 import os
 import argparse
 
@@ -26,8 +24,10 @@ def training(config_path):
     EPOCHS = config["params"]["epochs"]
     VALIDATION_SET = (X_valid, y_valid)
 
+    CALLBACK_LIST = get_callbacks(config, X_train)
+
     history = model.fit(X_train, y_train, epochs=EPOCHS,
-                        validation_data=VALIDATION_SET)
+                        validation_data=VALIDATION_SET, callbacks=CALLBACK_LIST)
 
     artifacts_dir = config['artifacts']['artifacts_dir']
     model_dir = config['artifacts']['model_dir']
